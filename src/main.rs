cda32301174e7dba0fd4c28d4e9694785db714e3
Manuel Graf <postmaster@yakbarber.org>
2024-01-10 01:14:17 +0100

    update ideas with parked old stuff

--- src/main.rs
+++ src/main.rs
@@ -1,261 +1,75 @@
-use clap::{ArgGroup, Parser, Subcommand};
-use clipboard::{ClipboardContext, ClipboardProvider};
-use exomem::{add_link, exomem_dir_path, find_md_file_paths, process_files, url_to_markdown_link};
-use rustyline::completion::{Completer, Pair};
-use rustyline::error::ReadlineError;
-use rustyline::highlight::Highlighter;
-use rustyline::hint::HistoryHinter;
-use rustyline::validate::Validator;
-use rustyline::Editor;
-use rustyline::{CompletionType, Config, Context, EditMode};
-use rustyline_derive::{Helper, Hinter};
-use std::collections::HashSet;
-use std::error::Error;
+// TODO: https://github.com/emilk/egui/discussions/1778
 
-const PROMPT_OK: &str = "\u{2713}";
-const PROMPT_ERR: &str = "\u{2717}";
-const PROMPT_BRAIN: &str = "\u{1f9e0}";
+use eframe::egui;
+use egui_plot;
 
-#[derive(Helper, Hinter)]
-struct TagHelper {
-    #[rustyline(Hinter)]
-    hinter: HistoryHinter,
-    tags: Vec<String>,
+use egui_plot::{Legend, MarkerShape, Plot, Points};
+
+use fdg_sim::{ForceGraph, ForceGraphHelper, Simulation, SimulationParameters};
+
+fn main() {
+    eframe::run_native(
+        "exomem",
+        eframe::NativeOptions::default(),
+        Box::new(|c| Box::new(App::new(c))),
+    );
 }
 
-impl Completer for TagHelper {
-    type Candidate = Pair;
-    fn complete(
-        &self,
-        line: &str,
-        pos: usize,
-        _ctx: &Context<'_>,
-    ) -> rustyline::Result<(usize, Vec<Pair>)> {
-        if line.is_empty() {
-            let candidates = self
-                .tags
-                .iter()
-                .map(|tag| Pair {
-                    display: tag.to_string(),
-                    replacement: tag.to_string(),
-                })
-                .collect();
-            return Ok((0, candidates));
+struct App {
+    v: Vec<[f64; 2]>,
+}
+
+impl Default for App {
+    fn default() -> App {
+        let mut graph: ForceGraph<(), ()> = ForceGraph::default();
+        let one = graph.add_force_node("one", ());
+        let two = graph.add_force_node("two", ());
+        let _three = graph.add_force_node("three", ());
+        graph.add_edge(one, two, ());
+        let mut simulation = Simulation::from_graph(graph, SimulationParameters::default());
+        for frame in 0..20 {
+            simulation.update(0.035);
         }
-        // match commands
-        if line.starts_with(":") {
-            let commands = vec![":add", ":format"];
-            let candidates = commands
-                .iter()
-                .filter(|c| c.starts_with(line))
-                .map(|c| Pair {
-                    display: c.to_string(),
-                    replacement: c.to_string(),
-                })
-                .collect();
-            return Ok((pos - line.len(), candidates));
+        App {
+            v: simulation
+                .get_graph()
+                .node_weights()
+                .map(|node| [node.location.x as f64, node.location.y as f64])
+                .collect::<Vec<[f64; 2]>>(),
         }
-        // match tags
-        let query: Vec<&str> = line.split("+").collect::<Vec<&str>>();
-        if query.len() > 1 {
-            let search = query.last().unwrap();
-            let candidates = self
-                .tags
-                .iter()
-                .filter(|tag| tag.starts_with(search))
-                .map(|tag| Pair {
-                    display: tag.to_string(),
-                    replacement: tag.to_string(),
-                })
-                .collect();
-            return Ok((pos - search.len(), candidates));
-        }
-        let candidates = self
-            .tags
+    }
+}
+
+impl App {
+    fn new(c: &eframe::CreationContext<'_>) -> Self {
+        Self::default()
+    }
+
+    fn nodes(&self) -> Vec<Points> {
+        self.v
             .iter()
-            .filter(|tag| tag.starts_with(line))
-            .map(|tag| Pair {
-                display: tag.to_string(),
-                replacement: tag.to_string(),
+            .map(|v| {
+                Points::new(*v)
+                    .name("nodes")
+                    .filled(false)
+                    .radius(5.0)
+                    .shape(MarkerShape::Circle)
             })
-            .collect();
-        Ok((0, candidates))
+            .collect()
     }
 }
 
-impl Highlighter for TagHelper {}
-
-impl Validator for TagHelper {}
-
-fn format_link(link_line: &str) -> Option<String> {
-    match url_to_markdown_link(&link_line) {
-        Some(link) => {
-            println!("{PROMPT_OK} {}", link);
-            let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
-            ctx.set_contents(link.to_owned()).unwrap();
-            Some(link)
-        }
-        None => {
-            println!("{PROMPT_ERR} Formating failed");
-            None
-        }
-    }
-}
-
-fn gui() {
-    std::process::Command::new("emg")
-        .spawn()
-        .expect("Launching GUI failed");
-}
-
-fn cli(args: Args) -> Result<(), clap::Error> {
-    match &args.command {
-        Some(Commands::Format { link, store }) => {
-            if *store {
-                if let Some(formated_link) = format_link(link) {
-                    match add_link(formated_link.to_string()) {
-                        Ok(file_name) => println!("{PROMPT_OK} {file_name}"),
-                        Err(err) => println!("{PROMPT_ERR} {:?}", err),
-                    }
+impl eframe::App for App {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            let plot = Plot::new("nodes")
+                .data_aspect(1.0)
+                .legend(Legend::default());
+            plot.show(ui, |plot_ui| {
+                for node in self.nodes() {
+                    plot_ui.points(node);
                 }
-            } else {
-                format_link(link);
-            }
-        }
-        None => {
-            return Err(clap::Error::new(
-                clap::error::ErrorKind::MissingRequiredArgument,
-            ));
-        }
+            });
+        });
     }
-    Ok(())
-}
-
-#[derive(Parser, Debug)]
-#[command(version)]
-#[command(group(ArgGroup::new("mode").required(false).args(["repl", "gui", "cli"])))]
-struct Args {
-    #[arg(short, long, default_value_t = true)]
-    repl: bool,
-    #[arg(short, long, default_value_t = false)]
-    gui: bool,
-    #[arg(default_value_t = String::from("~/exomem.d"))]
-    dir: String,
-    #[arg(short, long, default_value_t = false)]
-    cli: bool,
-    #[command(subcommand)]
-    command: Option<Commands>,
-}
-
-#[derive(Subcommand, Debug)]
-enum Commands {
-    Format {
-        link: String,
-        #[arg(short, long, default_value_t = false)]
-        store: bool,
-    },
-}
-
-fn main() -> Result<(), Box<dyn Error>> {
-    let args = Args::parse();
-    if args.cli {
-        // Subcommand required for CLI mode
-        if let Err(err) = cli(args) {
-            err.print().expect("Printing error failed");
-        }
-        return Ok(());
-    }
-    if args.gui {
-        gui();
-        return Ok(());
-    }
-    let exomem_dir_path = exomem_dir_path()?;
-    let file_paths = find_md_file_paths(exomem_dir_path.as_path());
-    // TODO: use _files_with_tags for graph export
-    let (_files_with_tags, tags_with_files) = process_files(file_paths);
-    let mut rl = Editor::with_config(
-        Config::builder()
-            .history_ignore_space(true)
-            .completion_type(CompletionType::List)
-            .edit_mode(EditMode::Emacs)
-            .build(),
-    )?;
-    let mut helper_tags = tags_with_files
-        .keys()
-        .map(|key| key.to_string())
-        .collect::<Vec<String>>();
-    helper_tags.sort_unstable();
-    rl.set_helper(Some(TagHelper {
-        hinter: HistoryHinter {},
-        tags: helper_tags,
-    }));
-    loop {
-        let readline = rl.readline(PROMPT_BRAIN);
-        match readline {
-            Ok(line) => {
-                if line.starts_with(":add ") {
-                    if let Some(link_line) = line.splitn(2, " ").nth(1) {
-                        match add_link(link_line.to_string()) {
-                            Ok(file_name) => println!("{PROMPT_OK} {file_name}"),
-                            Err(err) => println!("{PROMPT_ERR} {:?}", err),
-                        }
-                    }
-                    continue;
-                }
-                if line.starts_with(":format ") {
-                    if let Some(link_line) = line.splitn(2, " ").nth(1) {
-                        format_link(link_line);
-                        continue;
-                    }
-                }
-
-                let query: Vec<&str> = line.split("+").collect();
-                if query.len() > 1 {
-                    let tag0 = query.get(0).unwrap();
-                    let mut result_files: HashSet<String> = tags_with_files
-                        .get(*tag0)
-                        .unwrap()
-                        .iter()
-                        .map(|file| file.to_string())
-                        .collect();
-                    for tag in &query[1..] {
-                        let other_files: HashSet<String> = tags_with_files
-                            .get(*tag)
-                            .unwrap()
-                            .iter()
-                            .map(|file| file.to_string())
-                            .collect();
-                        result_files = result_files
-                            .intersection(&other_files)
-                            .map(|file| file.to_string())
-                            .collect();
-                    }
-                    for file in result_files {
-                        println!("{file}");
-                    }
-                    continue;
-                }
-                match tags_with_files.get(&line) {
-                    Some(files) => {
-                        for file in files {
-                            println!("{file}");
-                        }
-                    }
-                    None => {
-                        println!("Tag not found")
-                    }
-                }
-            }
-            Err(ReadlineError::Interrupted) => {
-                break;
-            }
-            Err(ReadlineError::Eof) => {
-                break;
-            }
-            Err(err) => {
-                println!("{PROMPT_ERR} {err:?}");
-            }
-        }
-    }
-    Ok(())
 }
